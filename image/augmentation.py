5e6b296c422f2614e3ab2f696946016d04b4fc8b
taashi-s <x7286x_ayk@yahoo.co.jp>
2019-04-04 11:01:01 +0900

    Update augmentation.py

--- image/augmentation.py
+++ image/augmentation.py
@@ -4,13 +4,17 @@ import math
 import cv2
 import numpy as np
 import PIL.Image as Image
+from tqdm import tqdm
 
 
-ORIGIN_INPUTS_DIR = os.path.join('.', 'Inputs', 'inputs')
-ORIGIN_TEACHERS_DIR = os.path.join('.', 'Inputs', 'teachers')
+DATA_TYPE = 'valid'
 
-INPUTS_DIR = os.path.join('.', 'Outputs', 'inputs')
-TEACHERS_DIR = os.path.join('.', 'Outputs', 'teachers')
+BASE_DIR = os.path.join('.', 'origin')
+ORIGIN_INPUTS_DIR = os.path.join(BASE_DIR, DATA_TYPE, 'inputs')
+ORIGIN_TEACHERS_DIR = os.path.join(BASE_DIR, DATA_TYPE, 'teachers')
+
+INPUTS_DIR = os.path.join('.', DATA_TYPE + '_inputs')
+TEACHERS_DIR = os.path.join('.', DATA_TYPE + '_teachers')
 
 
 def main():
@@ -18,6 +22,8 @@ def main():
     files.sort()
     print('### target annotation file : ', len(files))
     print('')
+    counts = []
+    pbar = tqdm(total=len(files), desc="Augmentaion", unit=" Files")
     for file in files:
         file_name, _ = os.path.splitext(os.path.basename(file))
         teacher_path = os.path.join(ORIGIN_TEACHERS_DIR, file_name + '.png')
@@ -29,26 +35,53 @@ def main():
         if teacher_img is None:
             print('teacher_img is None : ', teacher_path)
             continue
-        image_data_augmentaiton(file_name, input_img, teacher_img)
+        count = image_data_augmentaiton(file_name, input_img, teacher_img
+                                        #, flip_type=1
+                                        #, scales=[0.8, 1.2]
+                                        , rotate_stride=2
+                                        , rotate_range=10
+                                       )
+        counts.append(count)
+        pbar.update(1)
+    pbar.close()
+
+    if max(counts) == min(counts):
+        print('')
+        print('### augmentated all data : 1 --> ', max(counts))
+    else:
+        for count, file in zip(files, counts):
+            file_name, _ = os.path.splitext(os.path.basename(file))
+            print('### augmentate %s : 1 --> ' % file_name, count)
 
 
-def image_data_augmentaiton(file_name, input_img, teacher_img):
+
+
+def image_data_augmentaiton(file_name, input_img, teacher_img
+                            , flip_type=None, scales=None, rotate_stride=None, rotate_range=None):
     bases = []
     bases.append((file_name + '_N', input_img, teacher_img))
-    bases.append((file_name + '_F', cv2.flip(input_img, 0), cv2.flip(teacher_img, 0)))
+    if flip_type is not None:
+        bases.append((file_name + '_F', cv2.flip(input_img, flip_type), cv2.flip(teacher_img, flip_type)))
 
     bases_2 = []
+    if scales is None:
+        scales = []
     for name, inp_img, tea_img in bases:
         bases_2.append((name + '_100', inp_img, tea_img))
-        scales = [0.5, 1.2, 1.5]
         for scale in scales:
             bases_2.append((name + '_%03d' % (scale * 100), scaling_image(inp_img, scale), scaling_image(tea_img, scale)))
 
     bases_3 = []
+    if rotate_stride is None:
+        rotate_stride = 360
+    if rotate_range is None:
+        rotate_range = 180
     for name, inp_img, tea_img in bases_2:
         bases_3.append((name + '_000', inp_img, tea_img))
-        for k in range(35):
-            deg = 10 * (k + 1)
+        for k in range((360 // rotate_stride) - 1):
+            deg = rotate_stride * (k + 1)
+            if rotate_range < deg and deg < 360 - rotate_range:
+                continue
             bases_3.append((name + '_%03d' % deg, rotate_image(inp_img, deg), rotate_image(tea_img, deg)))
 
     data = bases_3
@@ -58,7 +91,8 @@ def image_data_augmentaiton(file_name, input_img, teacher_img):
         teacher_save_path = os.path.join(TEACHERS_DIR, name + '.png')
         cv2.imwrite(input_save_path, inp_img)
         cv2.imwrite(teacher_save_path, tea_img)
-    print('### augmentation ', file_name, ' (1 -> ', len(data), ')')
+#    print('### augmentation ', file_name, ' (1 -> ', len(data), ')')
+    return len(data)
 
 
 def scaling_image(img, scale):
@@ -66,14 +100,14 @@ def scaling_image(img, scale):
     res_h, res_w = (math.floor(h * scale), math.floor(w * scale))
     img_resize = cv2.resize(img, (res_h, res_w))
     if scale < 1:
-        #img_scale = np.zeros(np.shape(img))
-        #pos_top = (h - res_h) // 2
-        #pos_left = (w - res_w) // 2
-        #img_scale[pos_top:pos_top+res_h,pos_left:pos_left+res_w,:] = img_resize
-        h_tile_num = __calculate_tile_num(h, res_h)
-        w_tile_num = __calculate_tile_num(w, res_w)
-        img_tile = np.tile(img_resize, (h_tile_num, w_tile_num, 1))
-        img_scale = __crop_image_center(img_tile, h, w)
+        img_scale = np.zeros(np.shape(img))
+        pos_top = (h - res_h) // 2
+        pos_left = (w - res_w) // 2
+        img_scale[pos_top:pos_top+res_h,pos_left:pos_left+res_w,:] = img_resize
+#        h_tile_num = __calculate_tile_num(h, res_h)
+#        w_tile_num = __calculate_tile_num(w, res_w)
+#        img_tile = np.tile(img_resize, (h_tile_num, w_tile_num, 1))
+#        img_scale = __crop_image_center(img_tile, h, w)
     elif scale > 1:
         img_scale = __crop_image_center(img_resize, h, w)
     else:
@@ -94,12 +128,12 @@ def __calculate_tile_num(org_size, res_size):
 
 
 def rotate_image(img, degree):
-    img_tile = np.tile(img, (3, 3, 1))
+    img_tile = img #np.tile(img, (3, 3, 1))
     pil_img_tile = Image.fromarray(np.uint8(img_tile))
     pil_img_tile_rotate = pil_img_tile.rotate(degree)
     img_tile_rotate = np.array(pil_img_tile_rotate)
 
-    img_rotate = __crop_image_center(img_tile_rotate, *(np.shape(img)[:2]))
+    img_rotate = img_tile_rotate # __crop_image_center(img_tile_rotate, *(np.shape(img)[:2]))
     return img_rotate
 
 
